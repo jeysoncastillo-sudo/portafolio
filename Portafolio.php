914d3be4d717c0eceba658ada086c651a1ccc756
jeysoncastillo-sudo <jeyson.castillo@unachi.ac.pa>
2025-11-17 16:28:09 -0500

    Add files via upload

---
+++ Portafolio.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+Cover Page 
+Fu
+Table of Contents
+Introduction: Short personal introduction (background,academic interests) 
+
+
+Virtual
+Student Portfolio
+
+Cover Page
+*Full name *Student ID *Program / Faculty *Course name *Professor’s name *Semester / Year Title: Learning Portfolio.
+
+Table of Contents
+
+Introduction:
+*Short personal introduction (background, academic interests). *Purpose of the portfolio: why it’s being created and what it will show.
+
+Evidence of Learning (Organized by weeks, units, or competencies)
+
+For each entry:
+
+Title of Evidence (e.g., Quiz, Essay, Project, Reflection, Presentation).
+
+Date Submitted.
+
+Description: short explanation of the task.
+
+Artifact: attach file, image, screenshot, or text excerpt.
+
+Reflection: what I learned, challenges, how I improved.
+
+Skills & Competencies Developed
+
+Technical skills (e.g., coding, lab reports, research, data analysis).
+
+Academic skills (e.g., writing, presenting, teamwork).
+
+Personal skills (e.g., time management, problem-solving).
+
+Self-Reflection
+
+What was my biggest achievement this semester?
+
+Which activity challenged me most?
+
+What will I do differently next time?
+
+Conclusion
+
+Summary of my progress.
+
+Connection to career goals or future studies.
+
+Appendices
+
+Certificates
+
+Extra activities (seminars, workshops)
+
+Feedback from professors/peers
+
+
+
+</body>
+</html>
